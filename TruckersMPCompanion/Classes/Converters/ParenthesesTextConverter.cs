8bba88c8c05c7032ab528fcf190ab2e9d34d97f5
Václav Uruba <uruba@outlook.com>
2016-03-06 23:42:28 +0100

    – added a converter for adding parentheses – display the game name in the server list's entries

---
+++ TruckersMPCompanion/Classes/Converters/ParenthesesTextConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace TruckersMPApp.Classes.Converters
+{
+    class ParenthesesTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return String.Format("({0})", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
