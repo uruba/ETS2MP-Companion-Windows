d96f3a51e13dde1944b1d5d3f77b935813e57771
Václav Uruba <uruba@outlook.com>
2016-03-06 20:07:12 +0100

    – change the "online" text's colour to the current theme colour when true – tweaked the styles of the ListView

---
+++ TruckersMPCompanion/Classes/Converters/OnlineTextColourConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace TruckersMPApp.Classes.Converters
+{
+    class OnlineTextColourConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? App.Current.Resources["SystemControlHighlightAccentBrush"] : App.Current.Resources["SystemControlForegroundBaseHighBrush"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
