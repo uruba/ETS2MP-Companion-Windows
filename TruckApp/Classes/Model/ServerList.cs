1f70b35ce4cdb5d32f67e7e8788a64cab40bae62
Václav Uruba <uruba@outlook.com>
2016-03-13 17:55:03 +0100

    – let the ServerList act as a sort of viewmodel for the MainPage

--- TruckApp/Classes/Model/ServerList.cs
+++ TruckApp/Classes/Model/ServerList.cs
@@ -9,17 +9,83 @@ using TruckersMPApp.Classes.Model.Entities;
 
 namespace TruckersMPApp.Classes.Model
 {
-    class ServerList
+    class ServerList : INotifyPropertyChanged
     {
-        public delegate void fetchEventHandler();
-
-        public event fetchEventHandler beforeFetch;
-        public event fetchEventHandler afterFetch;
-
         public ObservableCollection<ServerInfo> ServerCollection
         {
             get;
             private set;
+        }
+
+        public ObservableCollection<ServerInfo> Servers
+        {
+            get
+            {
+                if (this.ServerCollection == null || String.IsNullOrEmpty(filterByGameName))
+                {
+                    return this.ServerCollection;
+                }
+
+                return new ObservableCollection<ServerInfo>(this.ServerCollection.Where(x => x.gameName.Contains(filterByGameName)));
+            }
+        }
+
+        public bool TextFilterByGameVisibility
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(filterByGameName) || EmptyListPlaceholderVisibility == true || TextLastUpdatedVisibility == false)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public bool EmptyListPlaceholderVisibility
+        {
+            get
+            {
+                return (this.Servers == null || this.Servers.Count == 0) && this.isLoading == false ?
+                    true :
+                    false;
+            }
+        }
+        public bool TextLastUpdatedVisibility
+        {
+            get
+            {
+                return (this.EmptyListPlaceholderVisibility || this.Servers == null || this.Servers.Count == 0) ?
+                    false :
+                    true;
+            }
+        }
+
+        private bool _isLoading;
+
+        public bool isLoading
+        {
+            get
+            {
+                return _isLoading;
+            }
+            private set
+            {
+                _isLoading = value;
+                OnPropertyChanged("isLoading");
+            }
+        }
+
+        public string filterByGameName
+        {
+            get
+            {
+                return (Windows.Storage.ApplicationData.Current.LocalSettings.Values["filterServerByGame"] ?? String.Empty)
+                    as string;
+            }
         }
 
         public DateTime lastUpdated
@@ -28,12 +94,23 @@ namespace TruckersMPApp.Classes.Model
             private set;
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
         public async Task<ObservableCollection<ServerInfo>> getServers()
         {
-            beforeFetch();
+            this.isLoading = true;
             this.ServerCollection = await ServerInfoFetcher.fetchServers();
             this.lastUpdated = DateTime.Now;
-            afterFetch();
+            this.isLoading = false;
             return this.ServerCollection;
         }
     }
--- TruckApp/Classes/UI/MainPage.xaml.cs
+++ TruckApp/Classes/UI/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TruckersMPApp.Classes.Converters;
 using TruckersMPApp.Classes.Model;
 using TruckersMPApp.Classes.Model.Entities;
 using TruckersMPApp.Classes.UI.Dialogs;
@@ -19,7 +20,6 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-// TODO: Clean up this mess of a code
 namespace TruckersMPApp
 {
     /// <summary>
@@ -27,44 +27,9 @@ namespace TruckersMPApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        #region Server list
+
         private ServerList _serverList;
-        public ObservableCollection<ServerInfo> Servers {
-            get
-            {
-                if (this._serverList.ServerCollection == null || String.IsNullOrEmpty(filterByGameName))
-                {
-                    return this._serverList.ServerCollection;
-                }
 
-                return new ObservableCollection<ServerInfo>(this._serverList.ServerCollection.Where(x => x.gameName.Contains(filterByGameName)));
-            }
-        }
-        #endregion
-
-        #region Text filter by a game name
-        private string filterByGameName
-        {
-            get
-            {
-                return (Windows.Storage.ApplicationData.Current.LocalSettings.Values["filterServerByGame"] ?? String.Empty) 
-                    as string;
-            }
-        }
-        public Visibility TextFilterByGameVisibility
-        {
-            get
-            {
-                if (String.IsNullOrEmpty(filterByGameName) || EmptyListPlaceholderVisibility == Visibility.Visible || TextLastUpdatedVisibility == Visibility.Collapsed)
-                {
-                    return Visibility.Collapsed;
-                } else
-                {
-                    return Visibility.Visible;
-                }
-            }
-        }
-        #endregion
         public DateTime LastUpdated
         {
             get
@@ -72,50 +37,11 @@ namespace TruckersMPApp
                 return this._serverList.lastUpdated;
             }
         }
-        private bool _isLoading;
-        public bool isLoading
-        {
-            get
-            {
-                return _isLoading;
-            }
-            private set
-            {
-                if (value)
-                {
-                    this.loadingProgressBar.Visibility = Visibility.Visible;
-                } else
-                {
-                    this.loadingProgressBar.Visibility = Visibility.Collapsed;
-                }
-                _isLoading = value;
-            }
-        }
-        public Visibility EmptyListPlaceholderVisibility
-        {
-            get
-            {
-                return (this.Servers == null || this.Servers.Count == 0) && this.loadingProgressBar.Visibility == Visibility.Collapsed ?
-                    Visibility.Visible :
-                    Visibility.Collapsed;
-            }
-        }
-        public Visibility TextLastUpdatedVisibility
-        {
-            get
-            {
-                return (this.EmptyListPlaceholderVisibility == Visibility.Visible || this.Servers == null || this.Servers.Count == 0) ?
-                    Visibility.Collapsed :
-                    Visibility.Visible;
-            }
-        }
 
         public MainPage()
         {
             this.InitializeComponent();
             this._serverList = new ServerList();
-            this._serverList.beforeFetch += new ServerList.fetchEventHandler(beforeRefresh);
-            this._serverList.afterFetch += new ServerList.fetchEventHandler(afterRefresh);
 
             this.refreshServers();
         }
@@ -128,17 +54,6 @@ namespace TruckersMPApp
         private async void refreshServers()
         {
             await this._serverList.getServers();
-        }
-
-        private void beforeRefresh()
-        {
-            this.isLoading = true;
-        }
-
-        private void afterRefresh()
-        {
-            this.isLoading = false;
-
             Bindings.Update();
         }
 
