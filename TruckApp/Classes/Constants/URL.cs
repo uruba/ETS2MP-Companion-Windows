e4e98d6dfc5c34d2ed0652d0cdf93e6b12c9fc21
Václav Uruba <uruba@outlook.com>
2016-03-19 21:49:34 +0100

    – added a MEETUP_LIST constant

--- TruckApp/Classes/Constants/URL.cs
+++ TruckApp/Classes/Constants/URL.cs
@@ -3,5 +3,6 @@
     class URL
     {
         public const string SERVER_LIST = "https://api.truckersmp.com/v2/servers";
+        public const string MEETUP_LIST = "http://truckersmp.targasoft.cz/api/v1/meetup-list";
     }
 }
